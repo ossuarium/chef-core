932bebf494efbce0d3c3b8e18aa0af31509ee7ca
Evan Sosenko <razorx@evansosenko.com>
2014-06-22 11:59:44 -0700

    Added patch to disable apache default site.

--- recipes/_apache_server.rb
+++ recipes/_apache_server.rb
@@ -10,3 +10,10 @@ if platform?('ubuntu') && node['platform_version'] == '14.04'
 end
 
 include_recipe 'apache2::default'
+
+# This is required to enable or disable the default site on Ubuntu 14.04.
+# @todo Remove this when [apache2 issue 146] is closed.
+# [apache2 issue 146]: https://github.com/onehealth-cookbooks/apache2/issues/146
+apache_site '000-default.conf' do
+  enable node['apache']['default_site_enabled']
+end
