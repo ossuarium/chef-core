90d64275e0d483056b94162c68e444c5568c02b0
Evan Sosenko <razorx@evansosenko.com>
2014-07-06 13:49:44 -0700

    Include _common_system in services recipe.

--- recipes/services.rb
+++ recipes/services.rb
@@ -3,6 +3,8 @@
 # Recipe:: services
 #
 
+include_recipe 'core::_common_system' if node['core']['common_system']
+
 node['core']['services'].each do |service|
   core_service service[:name] do
     action service[:action] if service[:action]
