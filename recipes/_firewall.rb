1a839ff727384aea705640c955665eeb2ff988df
Evan Sosenko <razorx@evansosenko.com>
2014-07-07 01:19:30 -0700

    Limit MySQL firewall rule to private network.

--- recipes/_firewall.rb
+++ recipes/_firewall.rb
@@ -46,5 +46,7 @@ end
 firewall_rule 'mysql' do
   protocol :tcp
   port node['mysql']['port'].to_i
+  interface node['core']['private_interface']
+  source PrivateNetwork.new(node).subnet
   action node['core']['servers']['mysql'] ? :allow : :reject
 end
